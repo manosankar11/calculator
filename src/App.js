ef2c17b060c0d1f1223f3ee378bf2d1a3199e174
muruganandham <manosankar2000@gmail.com>
2021-09-08 16:08:38 +0530

    local commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className='app'>
+      <layout>
+        <Layout />
+      </layout>
+    </div>
+  );
+}
+
+export default App;
